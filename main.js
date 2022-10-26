09819e36ae9164a0038418f7637aadf4a074071a
Constanza Delfino <conidelfino@gmail.com>
2022-10-26 16:47:20 -0300

    changes js

--- main.js
+++ main.js
@@ -12,8 +12,12 @@ si coloco
 const hex=[0,1,2,3,4,5,6,7,8,9,"A", "B", "C", "D", "E", "F"]
 let btn=document.querySelector('#btn')
 let body=document.querySelector('body')
+let colorname = document.querySelector('#colorname')
 
 const cambiaColor = () => {
-    body.style.backgroundColor='#FCBACB'
-    
+    body.style.backgroundColor='#FCBACB'        
+    colorname.innerHTML = "#FCBACB"
 }
+
+btn.addEventListener('click',cambiaColor)
+
