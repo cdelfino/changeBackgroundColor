e909a51570678b9a9c788ce69a53725cb17bfdd6
Constanza Delfino <conidelfino@gmail.com>
2022-10-26 16:29:27 -0300

    js

---
+++ main.js
@@ -0,0 +1,18 @@
+//DOM document object model
+
+/*
+let nombre=document.querySelector('#texto')
+let email=document.querySelector('#email')
+let edad=document.querySelector('#edad')
+
+si coloco 
+ email.value ----->  el value me retornará conidelfino@gmail.com
+ nombre.value ------> el value me retornara constanza*/
+
+const hex=[0,1,2,3,4,5,6,7,8,9,"A", "B", "C", "D", "E", "F"]
+let btn=document.querySelector('#btn')
+let body=document.querySelector('body')
+
+const cambiaColor = () => {
+    body.style.backgroundColor='#FCBACB'
+}
